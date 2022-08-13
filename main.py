812acd6b80ef38f0af32a950178a0ffa12e974a2
SathyaGnanakumar <64232090+SathyaGnanakumar@users.noreply.github.com>
2022-08-13 00:18:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ import tweepy
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 
-
-# API Key: fClQrR93ZjqR565Dd1dVvo3nJ
-# API Key Secret: UZpsUq8mpHyR8wY7MFQdPgqfafCyT0TncDFNx1IoIh3XbydWvV
-# Access Token: 1558297313600327680-sYhh9vn2coG861w72OwScLteJlSZd6
-# Access Token Secret: AxuSdamM0oZXW1cWGPEgg9zO3rR7UPj8dhddZBNczaKXr
-
 def get_weather_data(url):
     session = requests.Session()
     session.headers['User-Agent'] = USER_AGENT
@@ -47,10 +41,10 @@ if __name__ == "__main__":
     data = get_weather_data(URL)
 
     twitter_auth_keys = {
-        "consumer_key": "fClQrR93ZjqR565Dd1dVvo3nJ",
-        "consumer_secret": "UZpsUq8mpHyR8wY7MFQdPgqfafCyT0TncDFNx1IoIh3XbydWvV",
-        "access_token": "1558297313600327680-WgnqP3h4gmlkp97wIx1Ar0CKumg6kQ",
-        "access_token_secret": "9Wsrl77IFx7CvUWqlf5toN04ZFCLul6OGc2EceGAYxHiK"
+        "consumer_key": "CONSUMER KEY",
+        "consumer_secret": "CONSUMER SECRET KEY,
+        "access_token": "ACCESS TOKEN",
+        "access_token_secret": "ACCESS TOKEN SECRET"
     }
 
     auth = tweepy.OAuthHandler(
